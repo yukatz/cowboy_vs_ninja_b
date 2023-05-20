8f7291a57845c09e4f571786ac8c34a569476641
Yulia Katz <yukatz578@gmail.com>
2023-05-20 06:47:06 -0700

    team broblems

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -3,33 +3,49 @@
 namespace ariel
 {
     using namespace std;
-    Team::Team(Character *leader):leader(leader){
+    Team::Team(Character *leader) : leader(leader)
+    {
         add(leader);
     }
-    void Team::add(Character *member){
-        if(this->warriors.size() == 10){
-            throw runtime_error ("The team can't include more then 10 members");
+    void Team::add(Character *member)
+    {
+        if (this->warriors.size() == 10)
+        {
+            throw runtime_error("The team can't include more then 10 members");
         }
         this->warriors.push_back(member);
     }
-    void Team::attack(Team *enemy){
-
+    void Team::attack(Team *enemy)
+    {
+        
     }
-    int  Team::stillAlive(){
+    int Team::stillAlive()
+    {
         int count;
-        for(Character *member : this->warriors){
-            if(member->isAlive()){
+        for (Character *member : this->warriors)
+        {
+            if (member->isAlive())
+            {
                 count++;
             }
         }
         return count;
     }
-    void Team::print(){
-        for(Character *member : this->warriors){
-            if(member->isAlive()){
-                member->print();
+    void Team::print()
+    {
+        for (Character *member : this->warriors)
+        {
+            if (member->isAlive() && dynamic_cast<Ninja *> (member))
+            {
+                cout << member->print() << endl;
+            }
+        }
+        for (Character *member : this->warriors)
+        {
+            if (member->isAlive() && dynamic_cast<Cowboy *> (member))
+            {
+                cout << member->print() << endl;
             }
         }
     }
-
 }
